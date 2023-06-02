1343470db384edaa59473b9c2148f26f88d30e1c
VincentPeer <vincent.peer@heig-vd.ch>
2023-06-02 14:42:12 +0200

    Activity xml init

--- app/src/main/java/com/example/geofencing/MainActivity.kt
+++ app/src/main/java/com/example/geofencing/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.geofencing
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.example.geofencing.databinding.ActivityMainBinding
+import com.example.geofencing.ui.NewGeofenceActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createAlarm.setOnClickListener {
+            startActivity(Intent(this, NewGeofenceActivity::class.java))
+        }
     }
+
 }
