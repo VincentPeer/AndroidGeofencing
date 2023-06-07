e80a6666fb411d061f0d068bc563d33df494a976
VincentPeer <vincent.peer@heig-vd.ch>
2023-06-07 14:14:27 +0200

    Opening google maps OK

--- app/src/main/java/com/example/geofencing/ui/MapActivity.kt
+++ app/src/main/java/com/example/geofencing/ui/MapActivity.kt
@@ -1,12 +1,80 @@
 package com.example.geofencing.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.geofencing.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mMapView: MapView? = null
+
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        mMapView = findViewById<View>(R.id.map) as MapView
+        mMapView!!.onCreate(mapViewBundle)
+        mMapView!!.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+        mMapView?.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView!!.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mMapView!!.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMapView!!.onStop()
+    }
+
+    override fun onPause() {
+        mMapView!!.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mMapView!!.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView!!.onLowMemory()
     }
 }
--- app/src/main/java/com/example/geofencing/ui/NewGeofenceActivity.kt
+++ app/src/main/java/com/example/geofencing/ui/NewGeofenceActivity.kt
@@ -1,12 +1,23 @@
 package com.example.geofencing.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.geofencing.R
+import com.example.geofencing.databinding.ActivityMainBinding
+import com.example.geofencing.databinding.ActivityNewGeofenceBinding
 
 class NewGeofenceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewGeofenceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_geofence)
+
+        binding = ActivityNewGeofenceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openMapBtn.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
     }
 }
