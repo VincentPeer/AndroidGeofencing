549c90d2a5d2a931758ac5f709df0b920878f07b
VincentPeer <vincent.peer@heig-vd.ch>
2023-06-11 12:31:01 +0200

    Commit of app version with newGeofenceActivity layout ready

--- app/src/main/java/com/example/geofencing/ui/NewGeofenceActivity.kt
+++ app/src/main/java/com/example/geofencing/ui/NewGeofenceActivity.kt
@@ -4,12 +4,17 @@ import android.app.Notification
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.geofencing.GeofenceApp
 import com.example.geofencing.R
 import com.example.geofencing.databinding.ActivityNewGeofenceBinding
 import com.example.geofencing.model.GeofenceNotification
 
 class NewGeofenceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewGeofenceBinding
+    private val geofenceViewModel: GeofenceViewModel by viewModels {
+        GeofenceViewModel.GeofenceViewModelFactory((application as GeofenceApp).repository, application)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_geofence)
@@ -17,9 +22,10 @@ class NewGeofenceActivity : AppCompatActivity() {
         binding = ActivityNewGeofenceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.openMapBtn.setOnClickListener {
+        binding.selectPositionBtn.setOnClickListener {
             startActivity(Intent(this, MapActivity::class.java))
         }
+
 
     }
 }
